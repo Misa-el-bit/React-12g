b993ae12754681d54c194a6b0fe51f432870e8bb
Misa-El <mmancillaglz07@gmail.com>
2021-09-17 18:24:11 -0500

    semaforo

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,25 +1,48 @@
+// convertir este archivo en un componente de clase
+import React, { Component } from 'react'; // de toda la libreria de React solo traemos la parte de Component
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    this.state = { // se agrega estado 
+      etapa: ""
+    }
+    this.setEtapa = this.setEtapa.bind(this) // asociar el handler al constructor
+  }
+  // crear el handler
+  setEtapa( event ){
+    const value = event.target.value
+    console.log(value)
+    this.setState({ etapa: value})
+  }
+  render(){
+    const{ etapa } = this.state
+    return(
+      /*
+      <div className="App">
+      <h1>{6 > 5 ? "hola koders!": "adios"}</h1>
+      <ul>
+      {
+        ["uno", "dos","tres","cuatro"].map (item => <li>{item}</li>)
+      }
+      </ul>
+      </div>
+      */
+      <>
+        <input className= "control" type="radio" name="etapa" value="siga" onClick={this.setEtapa}/>
+        <label for= "">Siga</label>
+        <input className= "control" type="radio" name="etapa" value="precaución" onClick={this.setEtapa}/>
+        <label for= "">Precaución</label>
+        <input className= "control" type="radio" name="etapa" value="alto" onClick={this.setEtapa}/>
+        <label for= "">Alto</label>
+        <div className= "semaforo">
+          <div className= {`luz ${etapa}`}></div>
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;
